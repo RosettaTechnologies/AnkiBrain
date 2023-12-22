d87dbad4569ca27f3efd205b3e1caa0576293f11
Elan Shahrabani <shahrabani.elan@gmail.com>
2023-12-21 21:28:06 -0500

    Progress towards fixing clozes

--- webview/src/api/cards.js
+++ webview/src/api/cards.js
@@ -1,14 +1,14 @@
-import {pyGenerateCards} from "./PythonBridge/senders/pyGenerateCards";
-import {store, updateUser} from "./redux";
-import {addCards, setCards} from "./redux/slices/cards";
-import {isLocalMode} from "./user";
-import {setMakeCardsLoading} from "./redux/slices/makeCardsText";
-import {generateCardsRequest} from "./server-api/cards";
-import {errorToast, infoToast, successToast} from "./toast";
+import { pyGenerateCards } from "./PythonBridge/senders/pyGenerateCards";
+import { store, updateUser } from "./redux";
+import { addCard, addCards, setCards } from "./redux/slices/cards";
+import { isLocalMode } from "./user";
+import { setMakeCardsLoading } from "./redux/slices/makeCardsText";
+import { generateCardsRequest } from "./server-api/cards";
+import { errorToast, infoToast, successToast } from "./toast";
 import nlp from "compromise";
-import {addFailedCards} from "./redux/slices/failedCards";
-import {pyEditSetting} from "./PythonBridge/senders/pyEditSetting";
-import {postChat} from "./server-api/networking/chat";
+import { addFailedCards } from "./redux/slices/failedCards";
+import { pyEditSetting } from "./PythonBridge/senders/pyEditSetting";
+import { postChat } from "./server-api/networking/chat";
 
 function splitIntoSentences(text) {
   let doc = nlp(text);
@@ -50,6 +50,9 @@ async function handleCardsRawString(
     }
 
     dispatch(addCards(cards));
+
+    // TODO: needs to be removed, this only adds the currently made cards to temp cards rather than the entire deck in
+    //  the redux store.
     await pyEditSetting("tempCards", cards);
     successToast(
       "Made Flashcards",
@@ -61,47 +64,77 @@ async function handleCardsRawString(
     infoToast(
       "Some cards failed",
       "Some of your cards were generated improperly. " +
-      "They may require a slight modification to work. " +
-      'The malformed JSON strings have been placed in the "Failed Cards" tab.'
+        "They may require a slight modification to work. " +
+        'The malformed JSON strings have been placed in the "Failed Cards" tab.'
     );
-    // dispatch(
-    //   setAppAlertModal({
-    //     show: true,
-    //     header: "Cards JSON",
-    //     alertText: (
-    //       <Flex direction={"column"}>
-    //         <Text>
-    //           The AI failed to format your cards properly. The raw json output
-    //           from the AI is below, you can try editing it to fix the syntax.
-    //           You can later paste the fixed json into the "edit cards JSON"
-    //           field to create these cards. Make sure each card has fields for
-    //           "type: str", "tags: [str]". If basic type then the card should
-    //           have "front" and "back" fields, while if cloze it should have a
-    //           singular "text" field instead.
-    //         </Text>
-    //         <Textarea value={rawString} />
-    //       </Flex>
-    //     ),
-    //   })
-    // );
   }
 }
 
+function parseJsonArrayFromString(str) {
+  // This regular expression matches a JSON array
+  // It looks for anything starting with '[' and ending with ']'
+  // and captures any content in between, including nested arrays.
+  const regex = /\[.*?]/s;
+
+  const match = str.match(regex);
+  if (match) {
+    try {
+      // Parse the found JSON array
+      return JSON.parse(match[0]);
+    } catch (e) {
+      // Match is malformed.
+      return null;
+    }
+  } else {
+    // No array found.
+    return null;
+  }
+}
+
+function surroundKeywordsWithAsterisks(text, keywords) {
+  let modifiedString = text;
+
+  // Function to escape special characters for regex
+  function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+
+  keywords.forEach((word) => {
+    let regex = new RegExp(escapeRegExp(word), "gi");
+    modifiedString = modifiedString.replace(regex, "*$&*");
+  });
+
+  return modifiedString;
+}
+
 // TODO in progress
-function buildClozeCardQuery(text, nClozes, customPrompt, language = "English") {
+function buildClozeCardQuery(
+  text,
+  nClozes,
+  customPrompt,
+  language = "English"
+) {
   return `
-    Please pick ${nClozes} of the most important words or phrases in the following ${language} sentence.
+    Please pick ONLY ${nClozes} of the most important words or phrases in the following ${language} sentence.
+    These words or phrases should be the most salient items of the sentence that provide key context. 
             
     "${text}"
     
-    Please respond in ${language}. Please respond using valid JSON array syntax.
+    Please respond using valid JSON array syntax in ${language}.
     
     ${customPrompt ? "Additionally: " + customPrompt : ""}
     `;
 }
 
 // TODO in progress
-export async function generateClozeCards(text, nClozes, customPrompt = "", language = store.getState().language.value, dispatch = store.dispatch) {
+// Expects text to be a single sentence.
+export async function generateClozeCardFromSentence(
+  text,
+  nClozes,
+  customPrompt = "",
+  language = store.getState().language.value,
+  dispatch = store.dispatch
+) {
   if (isLocalMode()) {
     // TODO
   } else {
@@ -113,11 +146,49 @@ export async function generateClozeCards(text, nClozes, customPrompt = "", langu
       store.getState().appSettings.ai.llmModel,
       store.getState().appSettings.ai.temperature,
       store.getState().user.value.accessToken
-    )
+    );
 
-    if (res.status === 'success') {
+    if (res.status === "success") {
+      dispatch(updateUser(res.data.user));
       let rawString = res.data.response.content;
+
+      // GPT will hopefully return an array of keywords/keyphrases that we can create clozes from.
+      let keywords = parseJsonArrayFromString(rawString);
+      let finalClozeText = surroundKeywordsWithAsterisks(text, keywords);
+      finalClozeText = convertAsterisksToCloze(finalClozeText);
+
+      dispatch(
+        addCard({
+          text: finalClozeText,
+          type: "cloze",
+          tags: [],
+        })
+      );
+
+      // successToast(
+      //   "Created Cloze Card",
+      //   `Created a cloze card, used keywords: ${JSON.stringify(keywords)}`
+      // );
     }
+  }
+}
+
+export async function generateClozeCards(
+  text,
+  nClozes,
+  customPrompt,
+  language,
+  dispatch
+) {
+  let sentences = splitIntoSentences(text);
+  for (let sentence of sentences) {
+    await generateClozeCardFromSentence(
+      sentence,
+      nClozes,
+      customPrompt,
+      language,
+      dispatch
+    );
   }
 }
 
@@ -139,6 +210,12 @@ export async function generateCards(
         handleCardsRawString(cardsRawString, cardType, dispatch);
       }
     } else {
+      // TODO: remove this temp redirection for debugging
+      if (cardType === "cloze") {
+        await generateClozeCards(text, 2, customPrompt, language, dispatch);
+        return;
+      }
+
       let res = await generateCardsRequest(
         text,
         customPrompt,
